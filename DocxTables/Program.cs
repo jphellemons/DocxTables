a1ec9ac405e473175917b7be5c3f677544645603
jphellemons <info@jphellemons.nl>
2014-12-10 16:34:35 +0100

    All table design option in docx nuget package

---
+++ DocxTables/Program.cs
@@ -0,0 +1,26 @@
+﻿using Novacode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocxTables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (DocX document = DocX.Create("C:/Temp/tables.docx"))
+            {
+                foreach (TableDesign td in (TableDesign[])Enum.GetValues(typeof(TableDesign)))
+                {
+                    Table aTable = document.InsertTable(5, 5); // because office preview is also 5 x 5
+                    aTable.Design = td;
+                    aTable.Rows[0].Cells[0].Paragraphs.First().Append(td.ToString());
+                }
+                document.Save();
+            }
+        }
+    }
+}
